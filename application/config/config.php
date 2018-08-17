2a2d1a63579ddffff0ee2f7ff761fd2d5a70de67
aluno <aluno@GRVL301B-17>
2018-08-17 20:41:41 -0300

    upando pro umbler

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/meuProjetoFrame';
+$config['base_url'] = 'http://vitoraguirre-com.umbler.net';
+//$config['base_url'] = 'http://localhost/meuProjetoFrame';
 
 $config['index_page'] = '';
 
